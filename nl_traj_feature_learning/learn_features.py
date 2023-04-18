a147f65b438e3a511703467ad3f97625ddd859b1
Jeremy Tien <jeremy29tien@gmail.com>
2023-04-17 17:41:46 -0700

    add training code

--- nl_traj_feature_learning/learn_features.py
+++ nl_traj_feature_learning/learn_features.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 import numpy as np
 from bert.extract_features import run_bert
 
@@ -66,3 +67,75 @@ class NLTrajAutoencoder (nn.Module):
 
         output = (encoded_traj_a, encoded_traj_b, encoded_lang, decoded_traj_a, decoded_traj_b)
         return output
+
+
+def train(data, epochs):
+    #  use gpu if available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # load it to the specified device, either gpu or cpu
+    model = NLTrajAutoencoder().to(device)
+
+    # create an optimizer object
+    # Adam optimizer with learning rate 1e-3
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+    # mean-squared error loss
+    mse = nn.MSELoss()
+
+    # transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+    #
+    # train_dataset = torchvision.datasets.MNIST(
+    #     root="~/torch_datasets", train=True, transform=transform, download=True
+    # )
+    #
+    # test_dataset = torchvision.datasets.MNIST(
+    #     root="~/torch_datasets", train=False, transform=transform, download=True
+    # )
+    #
+    train_dataset = None
+    val_dataset = None
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=128, shuffle=True, num_workers=4, pin_memory=True
+    )
+
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=32, shuffle=False, num_workers=4
+    )
+
+    for epoch in range(epochs):
+        loss = 0
+        for batch_features, _ in train_loader:
+            # reshape mini-batch data to [N, 784] matrix
+            # load it to the active device
+            batch_features = batch_features.to(device)
+            traj_a, traj_b, lang = batch_features
+
+            # reset the gradients back to zero
+            # PyTorch accumulates gradients on subsequent backward passes
+            optimizer.zero_grad()
+
+            # compute reconstructions
+            output = model(batch_features)
+            encoded_traj_a, encoded_traj_b, encoded_lang, decoded_traj_a, decoded_traj_b = output
+
+            # compute training reconstruction loss
+            reconstruction_loss = mse(decoded_traj_a, traj_a) + mse(decoded_traj_b, traj_b)
+            distance_loss = F.cosine_similarity(encoded_traj_b - encoded_traj_a, encoded_lang)
+            train_loss = reconstruction_loss + distance_loss
+
+            # compute accumulated gradients
+            train_loss.backward()
+
+            # perform parameter update based on current gradients
+            optimizer.step()
+
+            # add the mini-batch training loss to epoch loss
+            loss += train_loss.item()
+
+        # compute the epoch training loss
+        loss = loss / len(train_loader)
+
+        # display the epoch training loss
+        print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, epochs, loss))
