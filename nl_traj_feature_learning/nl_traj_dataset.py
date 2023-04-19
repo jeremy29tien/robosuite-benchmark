335b37a2ae9b8c5bd2d90cd5149ac1f71f530d19
Jeremy Tien <jeremy29tien@gmail.com>
2023-04-18 22:11:40 -0700

    add custom dataset loading code

---
+++ nl_traj_feature_learning/nl_traj_dataset.py
@@ -0,0 +1,21 @@
+import torch
+from torch.utils.data import Dataset
+import json
+import numpy as np
+
+
+class NLTrajComparisonDataset(Dataset):
+    def __init__(self, nlcomp_file, traj_a_file, traj_b_file):
+        with open(nlcomp_file, 'rb') as f:
+            self.nlcomps = json.load(f)
+        self.traj_as = np.load(traj_a_file)
+        self.traj_bs = np.load(traj_b_file)
+
+    def __len__(self):
+        return len(self.nlcomps)
+
+    def __getitem__(self, idx):
+        traj_a = self.traj_as[idx, :, :]
+        traj_b = self.traj_bs[idx, :, :]
+        nlcomp = self.nlcomps[idx]
+        return traj_a, traj_b, nlcomp
