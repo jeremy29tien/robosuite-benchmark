b295823fc7fa61f8d6906989780abe029dbe635c
Jeremy Tien <jeremy29tien@gmail.com>
2023-04-23 21:58:43 -0700

    decrease number of rollouts per policy

--- get_policy_rollouts.sh
+++ get_policy_rollouts.sh
@@ -6,6 +6,6 @@ configs=(0-0-0-0-0  0-1-0-2-1  0-1-2-0-0  0-2-0-1-0  1-0-0-0-0  1-0-0-2-2  1-1-0
 
 for config in "${configs[@]}"; do
     python scripts/rollout.py --load_dir "log/runs/diverse-rewards/$config/LiftModded-Jaco-OSC-POSITION-SEED251/*/" \
-    --camera "frontview" --record_video --seed 251 --num_episodes 100 \
+    --camera "frontview" --record_video --seed 251 --num_episodes 5 \
     --output_dir "data/diverse-rewards/$config"
 done
