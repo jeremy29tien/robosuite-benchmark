44b3587fb3ecfdf2c7bda2f289e7ae374c1f9891
Jeremy Tien <jeremy29tien@gmail.com>
2023-04-24 23:21:44 -0700

    add batches

--- bert_preprocessing.py
+++ bert_preprocessing.py
@@ -17,26 +17,42 @@ for i, l in enumerate(nlcomps):
     batches[-1] = batches[-1] + l + "\n"
     # bert_input = bert_input + l + "\n"
 
-bert_outputs = []
+# bert_outputs = []
+bert_output_embeddings = []
 for bert_input in batches:
-    bert_outputs.append(b.run_bert(bert_input))
+    bert_output = b.run_bert(bert_input)
+    # bert_outputs.append(b.run_bert(bert_input))
+
+    # Loop over the batch
+    for i in range(len(bert_output)):
+        bert_output_words = bert_output[i]['features']  # bert_outputs[i]['features']
+        bert_output_embedding = []
+        for word_embedding in bert_output_words:
+            bert_output_embedding.append(word_embedding['layers'][0]['values'])
+        # NOTE: We average across timesteps (since BERT produces a per-token embedding).
+        bert_output_embedding = np.mean(np.asarray(bert_output_embedding), axis=0)
+        # print("bert_output_embedding:", bert_output_embedding.shape)
+        bert_output_embeddings.append(bert_output_embedding)
+    np.save('/home/jeremy/robosuite-benchmark/data/nl-traj/all-pairs/nlcomps.npy',
+            np.asarray(bert_output_embeddings))
+
 # bert_output = b.run_bert(bert_input)
 
-bert_output_embeddings = []
-# Loop over the batch
-for i, l in enumerate(nlcomps):
-    bert_output_words = bert_outputs[i // batch_size][i % batch_size]['features']  # bert_outputs[i]['features']
-    bert_output_embedding = []
-    for word_embedding in bert_output_words:
-        bert_output_embedding.append(word_embedding['layers'][0]['values'])
-    # NOTE: We average across timesteps (since BERT produces a per-token embedding).
-    bert_output_embedding = np.mean(np.asarray(bert_output_embedding), axis=0)
-    # print("bert_output_embedding:", bert_output_embedding.shape)
-    bert_output_embeddings.append(bert_output_embedding)
-    if i % batch_size == 0:
-        np.save('/home/jeremy/robosuite-benchmark/data/nl-traj/all-pairs/nlcomps.npy', np.asarray(bert_output_embeddings))
-
-bert_output_embeddings = np.asarray(bert_output_embeddings)
-print("bert_output_embeddings:", bert_output_embeddings.shape)
-
-np.save('/home/jeremy/robosuite-benchmark/data/nl-traj/all-pairs/nlcomps.npy', bert_output_embeddings)
+# bert_output_embeddings = []
+# # Loop over the batch
+# for i, l in enumerate(nlcomps):
+#     bert_output_words = bert_outputs[i // batch_size][i % batch_size]['features']  # bert_outputs[i]['features']
+#     bert_output_embedding = []
+#     for word_embedding in bert_output_words:
+#         bert_output_embedding.append(word_embedding['layers'][0]['values'])
+#     # NOTE: We average across timesteps (since BERT produces a per-token embedding).
+#     bert_output_embedding = np.mean(np.asarray(bert_output_embedding), axis=0)
+#     # print("bert_output_embedding:", bert_output_embedding.shape)
+#     bert_output_embeddings.append(bert_output_embedding)
+#     if i % batch_size == 0:
+#         np.save('/home/jeremy/robosuite-benchmark/data/nl-traj/all-pairs/nlcomps.npy', np.asarray(bert_output_embeddings))
+#
+# bert_output_embeddings = np.asarray(bert_output_embeddings)
+# print("bert_output_embeddings:", bert_output_embeddings.shape)
+#
+# np.save('/home/jeremy/robosuite-benchmark/data/nl-traj/all-pairs/nlcomps.npy', bert_output_embeddings)
