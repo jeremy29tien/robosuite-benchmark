6099411a4117a28e045462efff4827595305d0cc
Jeremy Tien <jeremy29tien@gmail.com>
2023-04-25 01:30:26 -0700

    undo

--- nl_traj_feature_learning/learn_features.py
+++ nl_traj_feature_learning/learn_features.py
@@ -114,8 +114,7 @@ class NLTrajAutoencoder (nn.Module):
 
 def train(seed, nlcomp_file, traj_a_file, traj_b_file, epochs, save_dir, preprocessed_nlcomps=False):
     #  use gpu if available
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = torch.device("cuda:0")  # TODO: this is a temp fix. 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("device:", device)
 
     # load it to the specified device, either gpu or cpu
