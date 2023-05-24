3c7711af159e0466461455369236d75df1e5fd1f
Jeremy Tien <jeremy29tien@gmail.com>
2023-05-23 23:47:04 -0700

    debug

--- nl_traj_feature_learning/qualitative_analysis.py
+++ nl_traj_feature_learning/qualitative_analysis.py
@@ -259,7 +259,7 @@ def run_accuracy_check(model, device, n_trajs, trajectories, nl_comps, nl_embedd
                 ref_traj_feature_values = [speed(ref_traj[t]) for t in range(len(ref_traj))]
                 target_traj_feature_values = [speed(target_traj[t]) for t in range(len(target_traj))]
                 print("nl_comp:", nl_comp)
-                print("nl_embedding:", nl_embedding)
+                # print("nl_embedding:", nl_embedding)
                 print("ref_traj speed:", np.mean(ref_traj_feature_values))
                 print("target_traj speed:", np.mean(target_traj_feature_values))
                 if np.mean(target_traj_feature_values) < np.mean(ref_traj_feature_values):
