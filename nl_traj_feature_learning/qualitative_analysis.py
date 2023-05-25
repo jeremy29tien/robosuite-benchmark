26446c41c378ab58bfaa000f4196b92e09459f6d
Jeremy Tien <jeremy29tien@gmail.com>
2023-05-25 15:30:32 -0700

    debug

--- nl_traj_feature_learning/qualitative_analysis.py
+++ nl_traj_feature_learning/qualitative_analysis.py
@@ -295,6 +295,9 @@ def run_accuracy_check(model, device, n_trajs, trajectories, nl_comps, nl_embedd
             elif len([adj for adj in robosuite.synthetic_comparisons.less_speed_adjs if adj in nl_comp]) > 0:
                 ref_traj_feature_values = [speed(ref_traj[t]) for t in range(len(ref_traj))]
                 target_traj_feature_values = [speed(target_traj[t]) for t in range(len(target_traj))]
+                print("ref_traj_feature_values:", ref_traj_feature_values)
+                print("target_traj_feature_values:", target_traj_feature_values)
+
                 print("nl_comp:", nl_comp)
                 # print("nl_embedding:", nl_embedding)
                 print("ref_traj speed:", np.mean(ref_traj_feature_values))
