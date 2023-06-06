aba772a4466636a8d5d76f4cd5a74e15074501bd
Jeremy Tien <jeremy29tien@gmail.com>
2023-06-05 17:27:42 -0700

    make changes

--- pbrl/run_aprel.py
+++ pbrl/run_aprel.py
@@ -11,24 +11,41 @@ from nl_traj_feature_learning.learn_features import NLTrajAutoencoder
 from nl_traj_feature_learning.nl_traj_dataset import NLTrajComparisonDataset
 
 from gpu_utils import determine_default_torch_device
+import argparse
 
 
-controller = "OSC_POSITION"
-controller_config = load_controller_config(default_controller=controller)
-env = suite.make(has_renderer=False, has_offscreen_renderer=False, use_object_obs=True, use_camera_obs=False,
-                 reward_shaping=True, controller_configs=controller_config)
+def make_gym_env(seed):
+    # Create robosuite env
+    env_kwargs = {
+        "control_freq": 20,
+        "env_name": "LiftModded",
+        "hard_reset": False,
+        "horizon": 500,
+        "ignore_done": True,
+        "reward_scale": 1.0,
+        "robots": [
+            "Jaco"
+        ]
+    }
+    controller = "OSC_POSITION"
+    controller_config = load_controller_config(default_controller=controller)
+    env = suite.make(**env_kwargs,
+                     has_renderer=False,
+                     has_offscreen_renderer=False,
+                     use_object_obs=True,
+                     use_camera_obs=False,
+                     reward_shaping=True,
+                     controller_configs=controller_config)
 
-# Create gym-compatible env
-gym_env = GymWrapper(env)
+    # Make it a gym-compatible env
+    gym_env = GymWrapper(env)
+    obs_dim = gym_env.observation_space.low.size
+    action_dim = gym_env.action_space.low.size
 
-obs_dim = gym_env.observation_space.low.size
-action_dim = gym_env.action_space.low.size
+    np.random.seed(seed)
+    gym_env.seed(seed)
 
-# env_name = 'MountainCarContinuous-v0'
-# gym_env = gym.make(env_name)
-
-np.random.seed(0)
-gym_env.seed(0)
+    return gym_env
 
 
 def load_model(model_path):
@@ -41,53 +58,82 @@ def load_model(model_path):
     return model, device
 
 
-def feature_func(traj):
-    """Returns the features of the given MountainCar trajectory, i.e. \Phi(traj).
+def run_aprel(gym_env, model_path):
+    encoder_model, device = load_model(model_path)
 
-    Args:
-        traj: List of state-action tuples, e.g. [(state0, action0), (state1, action1), ...]
+    def feature_func(traj):
+        """Returns the features of the given trajectory, i.e. \Phi(traj).
 
-    Returns:
-        features: a numpy vector corresponding the features of the trajectory
-    """
-    # TODO: Using the trained model, encode the trajectory to get our 16-dimensional 'feature' encoding.
+        Args:
+            traj: List of state-action tuples, e.g. [(state0, action0), (state1, action1), ...]
 
-    states = np.array([pair[0] for pair in traj])
-    actions = np.array([pair[1] for pair in traj[:-1]])
-    min_pos, max_pos = states[:, 0].min(), states[:, 0].max()
-    mean_speed = np.abs(states[:, 1]).mean()
-    mean_vec = [-0.703, -0.344, 0.007]
-    std_vec = [0.075, 0.074, 0.003]
-    return (np.array([min_pos, max_pos, mean_speed]) - mean_vec) / std_vec
+        Returns:
+            features: a numpy vector corresponding the features of the trajectory
+        """
+        traj = [t[0] + t[1] for t in traj]
+        traj = torch.unsqueeze(torch.as_tensor(traj, dtype=torch.float32, device=device), 0)
+        print("traj tensor:", traj.shape)
+        with torch.no_grad():
+            encoded_traj, _, _, _, _ = encoder_model((traj, traj, ""))
+            encoded_traj = encoded_traj.squeeze().detach().cpu().numpy()
 
+        # TODO: Could add a line that normalizes each feature in the embedding.
+        #  Not sure whether there's a nice clean way of computing the mean and
+        #  standard deviation of each feature though.
+        return encoded_traj
 
-env = aprel.Environment(gym_env, feature_func)
+    env = aprel.Environment(gym_env, feature_func)
 
-trajectory_set = aprel.generate_trajectories_randomly(env, num_trajectories=10,
-                                                      max_episode_length=300,
-                                                      file_name=env_name, seed=0)
-features_dim = len(trajectory_set[0].features)
+    # trajectory_set = aprel.generate_trajectories_randomly(env, num_trajectories=10,
+    #                                                       max_episode_length=300,
+    #                                                       file_name=env_name, seed=0)
 
-query_optimizer = aprel.QueryOptimizerDiscreteTrajectorySet(trajectory_set)
+    # TODO: take trajectories from our training/val data.
+    trajectory_set = None
 
-true_user = aprel.HumanUser(delay=0.5)
+    # TODO: modify features_dim to reflect the feature dimension (16).
+    #  This is later used in `aprel.util_funs.get_random_normalized_vector(features_dim)`
+    features_dim = len(trajectory_set[0].features)
 
-params = {'weights': aprel.util_funs.get_random_normalized_vector(features_dim)}
-user_model = aprel.SoftmaxUser(params)
-belief = aprel.SamplingBasedBelief(user_model, [], params)
-print('Estimated user parameters: ' + str(belief.mean))
+    query_optimizer = aprel.QueryOptimizerDiscreteTrajectorySet(trajectory_set)
 
-query = aprel.PreferenceQuery(trajectory_set[:2])
+    true_user = aprel.HumanUser(delay=0.5)
 
-for query_no in range(10):
-    queries, objective_values = query_optimizer.optimize('mutual_information', belief, query)
-    print('Objective Value: ' + str(objective_values[0]))
-
-    responses = true_user.respond(queries[0])
-
-    # Erdem's fix:
-    # belief.update(aprel.Preference(queries[0], responses[0]))
-    initial_sampling_param = {"weights": [0 for _ in range(features_dim)]}
-    belief.update(aprel.Preference(queries[0], responses[0]), initial_point=initial_sampling_param)
-
+    params = {'weights': aprel.util_funs.get_random_normalized_vector(features_dim)}
+    user_model = aprel.SoftmaxUser(params)
+    belief = aprel.SamplingBasedBelief(user_model, [], params)
     print('Estimated user parameters: ' + str(belief.mean))
+
+    query = aprel.PreferenceQuery(trajectory_set[:2])
+
+    for query_no in range(10):
+        queries, objective_values = query_optimizer.optimize('mutual_information', belief, query)
+        print('Objective Value: ' + str(objective_values[0]))
+
+        responses = true_user.respond(queries[0])
+
+        # Erdem's fix:
+        # belief.update(aprel.Preference(queries[0], responses[0]))
+        initial_sampling_param = {"weights": [0 for _ in range(features_dim)]}
+        belief.update(aprel.Preference(queries[0], responses[0]), initial_point=initial_sampling_param)
+
+        print('Estimated user parameters: ' + str(belief.mean))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+
+    parser.add_argument('--seed', type=int, default=0, help='')
+    parser.add_argument('--model-path', type=str, default='', help='')
+    # parser.add_argument('--id-mapping', action="store_true", help='')
+    # parser.add_argument('--val-split', type=float, default=0.1, help='')
+
+    args = parser.parse_args()
+
+    seed = args.seed
+    model_path = args.model_path
+
+    gym_env = make_gym_env(seed)
+
+    run_aprel(gym_env, model_path)
+
