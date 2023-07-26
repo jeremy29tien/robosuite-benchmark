d9140ab55eaea5b55dc754eeb973bed869cba8e9
Jeremy Tien <jeremy29tien@gmail.com>
2023-07-25 18:16:36 -0700

    debug finding trajectory wth best learned reward

--- pbrl/run_aprel.py
+++ pbrl/run_aprel.py
@@ -402,7 +402,7 @@ def run_aprel(seed, gym_env, model_path, human_user, traj_dir='', video_dir='', 
 
             # 2. Find trajectory with highest return under the learned reward.
             if args['query_type'] == 'preference' or args['query_type'] == 'nl_command':
-                learned_rewards = eval_user_model.reward(eval_user_model.params['trajectory_set'])
+                learned_rewards = eval_user_model.reward(trajectory_set)
                 best_traj_i = int(np.argmax(learned_rewards))
                 best_traj = eval_user_model.params['trajectory_set'][best_traj_i]
                 best_traj_path = best_traj.clip_path
